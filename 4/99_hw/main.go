007916b572e615445bd75768d05077f3b5c494ae
nikron173 <nikron17355@gmail.com>
2024-04-25 22:45:24 +0400

    Дописан полностью метода SearchServer

--- 4/99_hw/main.go
+++ 4/99_hw/main.go
@@ -4,173 +4,12 @@ import (
 	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"sort"
 	"strconv"
 	"strings"
-	"time"
 )
-
-type User struct {
-	Id        int    `xml:"id"`
-	FirstName string `xml:"first_name"`
-	LastName  string `xml:"last_name"`
-	Age       int    `xml:"age"`
-	About     string `xml:"about" json:"-"`
-}
-
-type Param struct {
-	Query      string
-	OrderField string
-	OrderBy    string
-	Limit      int
-	Offset     int
-}
-
-func (u User) String() string {
-	return fmt.Sprintf("\t{\"id\": %d, \"firstName\": %s, \"lastName\": %s, \"age\": %d},\n",
-		u.Id, u.FirstName, u.LastName, u.Age)
-}
-
-func SortSliceUserField(u *[]User, param string, orderBy string, limit int, offset int) error {
-	switch param {
-	case "Id":
-		{
-			sort.Slice((*u), func(i, j int) bool {
-				return (*u)[i].Id > (*u)[j].Id
-			})
-		}
-	case "Age":
-		{
-			sort.Slice((*u), func(i, j int) bool {
-				return (*u)[i].Age > (*u)[j].Age
-			})
-		}
-	case "Name":
-		{
-			sort.Slice((*u), func(i, j int) bool {
-				fullNameOne := (*u)[i].FirstName + " " + (*u)[i].LastName
-				fullNameSecond := (*u)[j].FirstName + " " + (*u)[j].LastName
-				return fullNameOne > fullNameSecond
-			})
-		}
-	default:
-		{
-			return &ApplicationError{
-				Code:    http.StatusBadRequest,
-				Message: "Error sort by field (Id, Name, Age)",
-				Time:    time.Now(),
-			}
-		}
-	}
-
-	switch orderBy {
-	case "desc":
-		{
-
-		}
-	case "asc":
-		{
-			rv := make([]User, len((*u)), len((*u)))
-			for i := len((*u)) - 1; i >= 0; i-- {
-				rv[len((*u))-i-1] = (*u)[i]
-			}
-			*u = rv
-		}
-	default:
-		{
-			return &ApplicationError{
-				Code:    http.StatusBadRequest,
-				Message: "Not valid order by parameter (asc/desc)",
-				Time:    time.Now(),
-			}
-		}
-	}
-	//по умолчанию - это убывание
-	if offset >= len(*u) {
-		*u = make([]User, 0)
-		return nil
-	}
-
-	if limit > 0 && limit <= len(*u) && (offset+1) > 0 && (offset+1) <= len(*u) {
-		if limit+offset > len(*u) {
-			// limitUsers := make([]User, len(*u)-limit, len(*u)-limit)
-			*u = (*u)[offset:len(*u)]
-		} else {
-			// limitUsers := make([]User, limit, limit)
-			*u = (*u)[offset : offset+limit]
-		}
-	}
-
-	return nil
-}
-
-type Row struct {
-	ListUsers []User `xml:"row"`
-}
-
-type ApplicationError struct {
-	Code    int       `json:"code"`
-	Message string    `json:"message"`
-	Time    time.Time `json:"time"`
-}
-
-func (e *ApplicationError) Error() string {
-	return fmt.Sprintf("code: %d, error: %s, time: %s",
-		e.Code, e.Message, e.Time)
-}
-
-func searchUser(param *Param) ([]User, error) {
-	row := &Row{}
-
-	file, err := os.Open("dataset.xml")
-	if err != nil {
-		return nil, &ApplicationError{
-			Code:    http.StatusInternalServerError,
-			Message: "Error internal server",
-			Time:    time.Now(),
-		}
-	}
-
-	defer file.Close()
-	fileContents, err := io.ReadAll(file)
-	if err != nil {
-		return nil, &ApplicationError{
-			Code:    http.StatusInternalServerError,
-			Message: "Error internal server",
-			Time:    time.Now(),
-		}
-	}
-
-	if err := xml.Unmarshal(fileContents, row); err != nil {
-		return nil, &ApplicationError{
-			Code:    http.StatusInternalServerError,
-			Message: "Error parsing xml to golang objects",
-			Time:    time.Now(),
-		}
-	}
-
-	findUsers := make([]User, 0)
-
-	if len(param.Query) == 0 {
-		findUsers = append(findUsers, row.ListUsers...)
-	} else {
-		for _, user := range row.ListUsers {
-			if strings.Contains(user.About, param.Query) ||
-				strings.Contains(user.FirstName, param.Query) ||
-				strings.Contains(user.LastName, param.Query) {
-				findUsers = append(findUsers, user)
-			}
-		}
-	}
-
-	if err := SortSliceUserField(&findUsers, param.OrderField, param.OrderBy, param.Limit, param.Offset); err != nil {
-		return nil, err
-	}
-	return findUsers, nil
-}
 
 func SearchServer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-type", "application/json")
@@ -178,9 +17,9 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		http.ErrNotSupported.Error()
 		return
 	}
-	param := new(Param)
+	param := new(SearchRequest)
 	param.OrderField = "Name"
-	param.OrderBy = "desc"
+	param.Limit = 10
 
 	var err error
 
@@ -188,106 +27,315 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 		param.Query = query
 	}
 	if orderField := r.FormValue("order_field"); orderField != "" {
-		param.OrderField = orderField
+		if orderField == "Id" || orderField == "Age" || orderField == "Name" {
+			param.OrderField = orderField
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			jsonErr, _ := json.Marshal(SearchErrorResponse{
+				Error: "Error parameter order field",
+			})
+			fmt.Fprintf(w, "%s", jsonErr)
+			return
+		}
 	}
-	if orderBy := r.FormValue("order_by"); orderBy != "" {
-		param.OrderBy = orderBy
+	if orderByStr := r.FormValue("order_by"); orderByStr != "" {
+		if param.OrderBy, err = strconv.Atoi(orderByStr); err != nil || param.OrderBy < -1 || param.OrderBy > 1 {
+			w.WriteHeader(http.StatusBadRequest)
+			jsonErr, _ := json.Marshal(SearchErrorResponse{
+				Error: "Error parameter order by",
+			})
+			fmt.Fprintf(w, "%s", jsonErr)
+			return
+		}
 	}
 	if limitStr := r.FormValue("limit"); limitStr != "" {
 		if param.Limit, err = strconv.Atoi(limitStr); err != nil || param.Limit < 0 {
-			appErr := &ApplicationError{
-				Code:    http.StatusBadRequest,
-				Message: "Error parameter limit",
-				Time:    time.Now(),
-			}
-			appErrJson, _ := json.Marshal(appErr)
-			w.WriteHeader(appErr.Code)
-			fmt.Fprintf(w, "%s", appErrJson)
+			w.WriteHeader(http.StatusBadRequest)
+			jsonErr, _ := json.Marshal(SearchErrorResponse{
+				Error: "Error parameter limit",
+			})
+			fmt.Fprintf(w, "%s", jsonErr)
 			return
 		}
 	}
 	if offsetStr := r.FormValue("offset"); offsetStr != "" {
 		if param.Offset, err = strconv.Atoi(offsetStr); err != nil || param.Offset < 0 {
-			appErr := &ApplicationError{
-				Code:    http.StatusBadRequest,
-				Message: "Error parameter offset",
-				Time:    time.Now(),
-			}
-			appErrJson, _ := json.Marshal(appErr)
-			w.WriteHeader(appErr.Code)
-			fmt.Fprintf(w, "%s", appErrJson)
+			w.WriteHeader(http.StatusBadRequest)
+			jsonErr, _ := json.Marshal(SearchErrorResponse{
+				Error: "Error parameter offset",
+			})
+			fmt.Fprintf(w, "%s", jsonErr)
 			return
 		}
 	}
 
-	users, err := searchUser(param)
+	file, err := os.Open("dataset.xml")
 	if err != nil {
-		if appErr, ok := err.(*ApplicationError); ok {
-			w.WriteHeader(appErr.Code)
-		} else {
-			w.WriteHeader(http.StatusBadGateway)
-		}
-		errJson, _ := json.Marshal(err)
-		fmt.Fprintf(w, "%s", errJson)
+		w.WriteHeader(http.StatusInternalServerError)
+		jsonErr, _ := json.Marshal(SearchErrorResponse{
+			Error: "Error internal server",
+		})
+		fmt.Fprintf(w, "%s", jsonErr)
 		return
 	}
 
-	jsonUsers, err := json.Marshal(users)
-	if err != nil {
-		if appErr, ok := err.(*ApplicationError); ok {
-			w.WriteHeader(appErr.Code)
-		} else {
-			w.WriteHeader(http.StatusBadGateway)
+	defer file.Close()
+	decoder := xml.NewDecoder(file)
+	users := make([]User, 0)
+	i := 0
+
+	for t, _ := decoder.Token(); t != nil; t, _ = decoder.Token() {
+		switch se := t.(type) {
+		case xml.StartElement:
+			switch se.Name.Local {
+			case "id":
+				{
+					users = append(users, User{})
+					decoder.DecodeElement(&users[i].Id, &se)
+				}
+			case "age":
+				{
+					decoder.DecodeElement(&users[i].Age, &se)
+				}
+			case "first_name":
+				{
+					decoder.DecodeElement(&users[i].Name, &se)
+				}
+			case "last_name":
+				{
+					lastName := ""
+					decoder.DecodeElement(&lastName, &se)
+					users[i].Name = users[i].Name + " " + lastName
+				}
+			case "gender":
+				{
+					decoder.DecodeElement(&users[i].Gender, &se)
+				}
+			case "about":
+				{
+					decoder.DecodeElement(&users[i].About, &se)
+					i++
+				}
+			}
 		}
-		errJson, _ := json.Marshal(err)
-		fmt.Fprintf(w, "%s", errJson)
+	}
+
+	findUsers := make([]User, 0)
+
+	if len(param.Query) == 0 {
+		findUsers = append(findUsers, users...)
+	} else {
+		for _, user := range users {
+			if strings.Contains(user.About, param.Query) ||
+				strings.Contains(user.Name, param.Query) {
+				findUsers = append(findUsers, user)
+			}
+		}
+	}
+
+	if param.OrderBy != 0 {
+		switch param.OrderField {
+		case "Id":
+			{
+				sort.Slice(findUsers, func(i, j int) bool {
+					if param.OrderBy == -1 {
+						return findUsers[i].Id > findUsers[j].Id
+					}
+					return findUsers[i].Id < findUsers[j].Id
+				})
+			}
+		case "Age":
+			{
+				sort.Slice(findUsers, func(i, j int) bool {
+					if param.OrderBy == -1 {
+						return findUsers[i].Age > findUsers[j].Age
+					}
+					return findUsers[i].Age < findUsers[j].Age
+				})
+			}
+		case "Name":
+			{
+				sort.Slice(findUsers, func(i, j int) bool {
+					if param.OrderBy == -1 {
+						return findUsers[i].Name > findUsers[j].Name
+					}
+					return findUsers[i].Name < findUsers[j].Name
+				})
+			}
+		}
+
+	}
+
+	response := &SearchResponse{}
+
+	if param.Offset >= len(findUsers) {
+		findUsers = make([]User, 0)
+		w.WriteHeader(http.StatusOK)
+		response.Users = findUsers
+		jsonResponse, _ := json.Marshal(response)
+		fmt.Fprintf(w, "%s", jsonResponse)
 		return
 	}
 
-	fmt.Fprintf(w, "%s", jsonUsers)
+	if param.Limit > 0 && param.Limit <= len(findUsers) && (param.Offset+1) > 0 && (param.Offset+1) <= len(findUsers) {
+		if param.Limit+param.Offset > len(findUsers) {
+			findUsers = findUsers[param.Offset:]
+		} else {
+			findUsers = findUsers[param.Offset : param.Offset+param.Limit]
+			response.NextPage = true
+		}
+	}
 
+	response.Users = findUsers
+	w.WriteHeader(http.StatusOK)
+	jsonResponse, _ := json.Marshal(response)
+	fmt.Fprintf(w, "%s", jsonResponse)
 }
 
 func main() {
-
-	// r := bufio.NewReader(os.Stdin)
-	// fmt.Println("Enter user name:")
-	// name, _ := r.ReadString('\n')
-	// name = strings.Replace(name, "\r\n", "", -1)
-	// fmt.Println("Enter user about:")
-	// about, _ := r.ReadString('\n')
-	// about = strings.Replace(about, "\r\n", "", -1)
-	// fmt.Println("Enter user order field:")
-	// orderField, _ := r.ReadString('\n')
-	// orderField = strings.Replace(orderField, "\r\n", "", -1)
-	// fmt.Println("Enter user order by (asc/desc):")
-	// orderBy, _ := r.ReadString('\n')
-	// orderBy = strings.Replace(orderBy, "\r\n", "", -1)
-	// fmt.Println("Enter user limit (int):")
-	// limitStr, _ := r.ReadString('\n')
-	// limit, err := strconv.Atoi(strings.Replace(limitStr, "\r\n", "", -1))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("Enter user offset (int):")
-	// offsetStr, _ := r.ReadString('\n')
-	// offset, err := strconv.Atoi(strings.Replace(offsetStr, "\r\n", "", -1))
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// param := &Param{}
-	// param.Name = name
-	// param.About = about
-	// param.OrderField = orderField
-	// param.OrderBy = orderBy
-	// param.Limit = limit
-	// param.Offset = offset
-
-	// fmt.Println(SearchUser(param))
-
 	http.HandleFunc("/users", SearchServer)
 
 	fmt.Println("Starting web server on port :8081")
 	http.ListenAndServe(":8081", nil)
 }
+
+func (u User) String() string {
+	return fmt.Sprintf("\t{\"id\": %d, \"name\": %s, \"age\": %d},\n",
+		u.Id, u.Name, u.Age)
+}
+
+// func SortField(u *[]User, orderField string, orderBy int) {
+// 	switch orderField {
+// 	case "Id":
+// 		{
+// 			sort.Slice((*u), func(i, j int) bool {
+// 				if orderBy == -1 {
+// 					return (*u)[i].Id > (*u)[j].Id
+// 				}
+// 				return (*u)[i].Id < (*u)[j].Id
+// 			})
+// 		}
+// 	case "Age":
+// 		{
+// 			sort.Slice((*u), func(i, j int) bool {
+// 				if orderBy == -1 {
+// 					return (*u)[i].Age > (*u)[j].Age
+// 				}
+// 				return (*u)[i].Age < (*u)[j].Age
+// 			})
+// 		}
+// 	case "Name":
+// 		{
+// 			sort.Slice((*u), func(i, j int) bool {
+// 				if orderBy == -1 {
+// 					return (*u)[i].Name > (*u)[j].Name
+// 				}
+// 				return (*u)[i].Name < (*u)[j].Name
+// 			})
+// 		}
+// 	}
+// }
+
+// func SortSliceUserField(u *[]User, orderField string, orderBy int, limit int, offset int) error {
+
+// 	if orderBy != 0 {
+// 		SortField(u, orderField, orderBy)
+// 	}
+
+// 	//по умолчанию - это убывание
+// 	if offset >= len(*u) {
+// 		*u = make([]User, 0)
+// 		return nil
+// 	}
+
+// 	if limit > 0 && limit <= len(*u) && (offset+1) > 0 && (offset+1) <= len(*u) {
+// 		if limit+offset > len(*u) {
+// 			*u = (*u)[offset:len(*u)]
+// 		} else {
+// 			*u = (*u)[offset : offset+limit]
+// 		}
+// 	}
+
+// 	return nil
+// }
+
+// type ApplicationError struct {
+// 	Code    int       `json:"code"`
+// 	Message string    `json:"message"`
+// 	Time    time.Time `json:"time"`
+// }
+
+// func (e *ApplicationError) Error() string {
+// 	return fmt.Sprintf("code: %d, error: %s, time: %s",
+// 		e.Code, e.Message, e.Time)
+// }
+
+// func SearchUser(param *SearchRequest) ([]User, error) {
+// 	file, err := os.Open("dataset.xml")
+// 	if err != nil {
+// 		return nil, &ApplicationError{
+// 			Code:    http.StatusInternalServerError,
+// 			Message: "Error internal server",
+// 			Time:    time.Now(),
+// 		}
+// 	}
+
+// 	defer file.Close()
+// 	decoder := xml.NewDecoder(file)
+// 	users := make([]User, 0)
+// 	i := 0
+
+// 	for t, _ := decoder.Token(); t != nil; t, _ = decoder.Token() {
+// 		switch se := t.(type) {
+// 		case xml.StartElement:
+// 			switch se.Name.Local {
+// 			case "id":
+// 				{
+// 					users = append(users, User{})
+// 					decoder.DecodeElement(&users[i].Id, &se)
+// 				}
+// 			case "age":
+// 				{
+// 					decoder.DecodeElement(&users[i].Age, &se)
+// 				}
+// 			case "first_name":
+// 				{
+// 					decoder.DecodeElement(&users[i].Name, &se)
+// 				}
+// 			case "last_name":
+// 				{
+// 					lastName := ""
+// 					decoder.DecodeElement(&lastName, &se)
+// 					users[i].Name = users[i].Name + " " + lastName
+// 				}
+// 			case "gender":
+// 				{
+// 					decoder.DecodeElement(&users[i].Gender, &se)
+// 				}
+// 			case "about":
+// 				{
+// 					decoder.DecodeElement(&users[i].About, &se)
+// 					i++
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	findUsers := make([]User, 0)
+
+// 	if len(param.Query) == 0 {
+// 		findUsers = append(findUsers, users...)
+// 	} else {
+// 		for _, user := range users {
+// 			if strings.Contains(user.About, param.Query) ||
+// 				strings.Contains(user.Name, param.Query) {
+// 				findUsers = append(findUsers, user)
+// 			}
+// 		}
+// 	}
+
+// 	if err := SortSliceUserField(&findUsers, param.OrderField, param.OrderBy, param.Limit, param.Offset); err != nil {
+// 		return nil, err
+// 	}
+// 	return findUsers, nil
+// }
